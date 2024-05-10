63970ae1992807c09957cd996bfc2b7efd0f38e3
christiansanchez-SE <christiansanchez714@gmail.com>
2024-05-09 22:26:55 -0400

    fifth commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 
-// Import our components
 import MovieDisplay from "./components/MovieDisplay";
 import Form from "./components/Form";
 
 export default function App() {
-  // Constant with your API key
-  const apiKey = "4b8c7626";
+  // Variable with your API Key
+  const apiKey = "98e3fb1f";
 
-  // State to hold the movie data
+  // State to hold movie data
   const [movie, setMovie] = useState(null);
 
   // Function to get movies
-  const getMovie = async(searchTerm) => {
+  const getMovie = async (searchTerm) => {
+    // Make fetch request and store the response
     const response = await fetch(
       `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
     );
@@ -24,12 +24,12 @@ export default function App() {
     setMovie(data);
   };
 
-
   // We pass the getMovie function as a prop called moviesearch
+  // We pass movie as props to movie display
   return (
     <div className="App">
-      <Form mvoiesearch ={getMovie}/>
-      <MovieDisplay />
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
     </div>
   );
 }
