e4a549d0e6db1ce750ec5e4161fcda48fe3ce0ca
christiansanchez-SE <christiansanchez714@gmail.com>
2024-05-09 22:02:18 -0400

    Third commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ import MovieDisplay from "./components/MovieDisplay";
 import Form from "./components/Form";
 
 export default function App() {
+  // Constant with your API key
+  const apiKey = "4b8c7626";
+
+  // State to hold the movie data
+  const [movie, setMovie] = useState(null);
+
+  // Function to get movies
+  const getMovie = async(searchTerm) => {
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    )
+  }
   return (
     <div className="App">
       <Form />
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,11 @@
-export default function Form(props){
-    // The component must return some JSX
-    return <h1>The Form Component</h1>;
-  };
+export default function Form(props) {
+  return (
+    <div>
+      <form>
+        <h1>The Form Component</h1>
+        <input type="text" />
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
+}
